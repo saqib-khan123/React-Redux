beaffbb687c56308739161a5091f86c59fa38f10
saqib-khan123 <saqibpathan776@gmail.com>
2023-12-30 16:18:21 +0530

    Add project to Bitbucket example

---
+++ src/containers/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="ui fixed menu" style={{background: '#cdcdcd'}}>
+      <div className="ui container center">
+        <h2 >FashionHub </h2>
+        {/* <img src="" alt="" /> */}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
